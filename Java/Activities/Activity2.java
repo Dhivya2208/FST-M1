b81e0afba1b97fd35435306e0c63b82919472317
Dhivya2208 <98084704+Dhivya2208@users.noreply.github.com>
2022-01-20 16:04:47 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,20 @@
+package Activities;
+
+public class Activity2 {
+    public static boolean calculate(int array[]){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]==10)
+                sum = sum + array[i];
+            if (sum == 30) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] array1 = new int[]{10, 77, 10, 54, -11, 10};
+        System.out.println(calculate(array1));
+
+    }
+}
